63bcb8fc4b93073dd913d7473ab09ad8fa99eeb3
Colombo <col553@csiro.au>
2020-05-08 16:45:38 +1000

    some playing around with functions and filter etc

--- script/More_tidy_batch.R
+++ script/More_tidy_batch.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+
 batch_01 <- read_csv("Data/LS_batch01.csv", col_types = cols(Name = col_character())) %>% 
   # rename(RT01 = RT)%>% 
   mutate(batch = "batch_01")
@@ -65,9 +66,10 @@ over_rep02 <- over_rep %>%
 ggplot(over_rep02, aes(meanRT, ExtractedMass)) +
   geom_point(aes(colour = batch, shape = Polarity)) +
   scale_shape_manual(values = c(6,16))
-
+ 
 # plot based on Classes
 
+unique(over_rep02$Class)
 
 class  <- over_rep02 %>% 
   filter(Class == "CL")
@@ -76,16 +78,116 @@ ggplot(class, aes(meanRT, ExtractedMass)) +
   geom_point(aes(colour = batch, shape = Polarity)) +
   scale_shape_manual(values = c(6,16))
 
+# trial to build the function
+plots_classes <-  over_rep02 %>% 
+  filter(Class == "SPH" ) %>%
+ ggplot(aes(meanRT, ExtractedMass)) +
+   geom_point(aes(colour = batch, shape = Polarity)) +
+   scale_shape_manual(values = c(6,16)) +
+  labs(title = "SPH") 
+  
+plots_classes
 
+# eliminate CL under 15 min
+plots_classes <-  over_rep02 %>% 
+  filter(Class == "CL") %>%
+  filter(!meanRT < 15 ) %>% 
+  ggplot(aes(meanRT, ExtractedMass)) +
+  geom_point(aes(colour = batch, shape = Polarity)) +
+  scale_shape_manual(values = c(6,16)) +
+  labs(title = "CL") 
+
+plots_classes
+
+# eliminate PC with NA adducts
+plots_classes <-  over_rep02 %>% 
+  filter(Class == "PC") %>%
+  filter(!Adduct =="Na-Gain" ) %>% 
+  ggplot(aes(meanRT, ExtractedMass)) +
+  geom_point(aes(colour = batch, shape = Polarity)) +
+  scale_shape_manual(values = c(6,16)) +
+  labs(title = "PC") 
+
+plots_classes
+
+# now plotting all lipids with some of the modifications above
+#filter(grepl( "CL", Class)) %>% 
+
+all_lipids <- over_rep02 %>% 
+    filter((Class =="CL" & meanRT > 15)|(Class =="PC" & !Adduct =="Na-Gain")) 
+
+       filter(over_rep02,
+              grepl("PC", Class)),
+              Adduct =="Na-Gain") %>%
+    ggplot(aes(meanRT, ExtractedMass)) +
+  geom_point(aes(colour = batch, shape = Polarity)) +
+  scale_shape_manual(values = c(6,16)) +
+  labs(title = "All lipids tidier version") 
+
+all_lipids
+#--------------------------------------------------------------------
 # this function below is wrong why?
-plot_class <-  function(x) {
-  over_rep02 %>% 
-    filter(Class == "x" ) %>% 
-  ggplot(class, aes(meanRT, ExtractedMass)) +
+# '''
+# No need to add an argument to the function with the object where you are going to store 
+# the ggplot output. 
+# 
+# Before you had function(df, y){
+# 
+# ... 
+# y <- ggplot()
+# }
+# 
+# that is not need it, you can always pass the results of ggplot into an object withouth defining it previously. 
+# That object would live withing the function. You can do this when you want to 
+# keep working on it and will create an output later.
+# 
+# or when you want to store the result of the function into another object with return
+# 
+# '''
+# this function below doesn't work
+plot_class <-  function(df) {
+    df <-  df %>% 
+     filter(Class == "CL" )
+    y <- ggplot(df, aes(meanRT, ExtractedMass)) +
+    geom_point(aes(colour = batch, shape = Polarity)) +
+    scale_shape_manual(values = c(6,16))
+    
+    return(y)
+}
+
+
+#same function below without object This works
+plot_class <-  function(df) {
+      df <-  df %>% 
+         filter(Class == "CL" )
+       y <- ggplot(df, aes(meanRT, ExtractedMass)) +
+         geom_point(aes(colour = batch, shape = Polarity)) +
+         scale_shape_manual(values = c(6,16)) + theme(plot.subtitle = element_text(vjust = 1), 
+    plot.caption = element_text(vjust = 1)) +
+         labs(title = "CL")
+       
+          # y.classic <- y + theme_classic()
+          # print(y)
+          # print(y.classic)
+     }
+
+
+plot_class(over_rep02)
+
+# -----------------------------------------------------
+# this function is only taking a couple of classses, not all of them 
+plot_class <-  function(data, var =c("x")) {
+  
+  data2 <- data %>% 
+    filter(Class %in% var)
+  
+  ggplot(data2, aes(meanRT, ExtractedMass)) +
     geom_point(aes(colour = batch, shape = Polarity)) +
     scale_shape_manual(values = c(6,16))
 }
 
-plot_class(PE)
+
+plot_class(over_rep02, var =c("FA"))
+
 
 
