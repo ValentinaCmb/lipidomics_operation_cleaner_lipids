35ceffe06571b2bbbc7ac1a94436edd8ece6d5a8
Valentina <Valentina.Colombo@csiro.au>
2020-05-11 14:33:13 +1000

    qcs script

--- script/Filtering_qcs_data.R
+++ script/Filtering_qcs_data.R
@@ -1,20 +1,17 @@
 library(tidyverse)
 
-batch_qcs <- read_csv("Data/CD_QCs_analysis.csv", col_types = cols(Name = col_character())) 
-  
-tidier_batch <- batch_01 %>% 
-  rbind(batch_02, batch_03, batch_04) %>% 
-  mutate(RT = round(RT, 1)) %>% 
-  select(-"X11", -Window) %>% 
-  arrange(CompoundName, RT) %>% 
-  rename(Name = CompoundName, Formula = ChemicalFormula) %>% 
+batch_qcs <- read_tsv("Data/CD_QCs_analysis.csv", col_types = cols(Name = col_character())) 
+
+CDdata_gathered <- batch_qcs %>% 
+  gather(Sample, Area, contains("Area: ")) %>%
   unique() %>% 
-  mutate(Class = str_extract(`Name`, "[a-zA-Z]+"))  
-# mutate(TC = word(`Name`, 1, 1, sep = "_"),
-#        DB = word(`Name`, -1, -1, sep = "_"))
+    filter(!is.na(Area)) %>% 
+  select("Name", 'Formula','RT [min]', 'Area (Max.)', 'Sample') %>% 
+  group_by(Name, `RT [min]`) %>% 
+  mutate(count = n()) %>% 
+  filter(count >= 10)
 
 
-cleaned_batch <- tidier_batch %>% 
-  mutate(Reject = case_when(Adduct == "Na-Gain" ~ "rej",
-                            Class == "CL" & RT < 10 ~ "rej",
-                            TRUE ~ ""))
+  unique('Sample')
+
+  group_by(Name, `RT [min]`)%>%
