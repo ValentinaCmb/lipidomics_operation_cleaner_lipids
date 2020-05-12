8f21e79c83023a581b8c625081f9c26578adc745
Matthew Taylor <matt@Matthews-iMac.local>
2020-05-12 12:14:11 +1000

    add TC and DB, clean adducts filter

--- script/More_tidy_batch.R
+++ script/More_tidy_batch.R
@@ -1,5 +1,26 @@
 library(tidyverse)
 
+extract_TC <- function(molecule){
+  lipidclass <- str_extract(molecule, "[a-zA-Z]+")
+  # df <- as.data.frame(str_extract_all(molecule, "\\d+[:]\\d+")) %>%
+  #   separate(1, c("C", "DB"), ":", convert = T) %>%
+  #   summarise(
+  #     Carbons = sum(C),
+  #     DB = sum(DB)
+  #   )
+  C <- sum(as.numeric(unlist(str_extract_all(molecule, "\\d+(?=\\:)"))))
+  DB <- sum(as.numeric((unlist(str_extract_all(molecule, "(?<=\\:)\\d+")))))
+  modification <- str_extract(str_extract(molecule, "(?<=\\().+?(?=\\))"), "[a-z]")
+  # species <- ifelse(modification == "t"|modification == "d", paste(modification, df$Carbons, ":", df$DB, sep = ""), 
+  #                   ifelse(modification == "e"| modification == "p", paste(df$Carbons, modification, ":", df$DB, sep = ""), 
+  #                   paste(df$Carbons, df$DB, sep = ":")))
+  
+  species <- case_when(modification == "t"|modification == "d" ~ paste(modification, C, ":", DB, sep = ""), 
+                       modification == "e"| modification == "p" ~  paste(C, ":", DB, modification, sep = ""), 
+                       TRUE ~ paste(C, DB, sep = ":"))
+  return(species)
+}
+
 batch_01 <- read_csv("Data/LS_batch01.csv", col_types = cols(Name = col_character())) %>% 
   # rename(RT01 = RT)%>% 
   mutate(batch = "batch_01")
@@ -27,12 +48,18 @@ tidier_batch <- batch_01 %>%
   arrange(CompoundName, RT) %>% 
   rename(Name = CompoundName, Formula = ChemicalFormula) %>% 
   unique() %>% 
-  mutate(Class = str_extract(`Name`, "[a-zA-Z]+"))  
+  group_by(Name, RT, batch, Adduct) %>% 
+  mutate(Class = str_extract(`Name`, "[a-zA-Z]+"),
+         species = extract_TC(Name)
+         # C = sum(as.numeric(unlist(str_extract_all(Name, "\\d+(?=\\:)")))),
+         # DB = sum(as.numeric((unlist(str_extract_all(Name, "(?<=\\:)\\d+")))))
+         )
+        
   # mutate(TC = word(`Name`, 1, 1, sep = "_"),
   #        DB = word(`Name`, -1, -1, sep = "_"))
 
 # plot based on batches
-ggplot(cleaned_batch, aes(RT, ExtractedMass)) +
+ggplot(tidier_batch, aes(RT, ExtractedMass)) +
   geom_point(aes(colour = batch, shape = Polarity)) +
   scale_shape_manual(values = c(6,16))
 
@@ -42,6 +69,7 @@ cleaned_batch <- tidier_batch %>%
                             ChargeState != 1 ~ "rej",
                             Adduct == "Na-Gain" ~ "rej",
                             Class == "CL" & RT < 15 ~ "rej",
+                            Class == "TG" & RT < 10 ~"rej",
                          TRUE ~ ""))
 
 # plot based on cleaned batches
@@ -49,31 +77,77 @@ ggplot(cleaned_batch %>% filter(Reject != "rej"), aes(RT, ExtractedMass)) +
   geom_point(aes(colour = batch, shape = Polarity)) +
   scale_shape_manual(values = c(6,16))
 
+
+#The adduct mass is reported - need to make this MW
+H <- 1.007825
+N <- 14.003074
+electron <- 0.000548#0.000548579909
+
 Adduct_spread <- cleaned_batch %>% 
   filter(Reject != "rej") %>% 
   select(Name, Class, Formula, batch, RT, ExtractedMass, Adduct) %>% 
   spread(Adduct, ExtractedMass) %>% 
-  mutate(MW = case_when(!is.na(`H-Gain`) ~ `H-Gain` - 1,
-                        !is.na(`H-Loss`) ~ `H-Loss` + 1,
-                        !is.na(`NH4-Gain`) ~ `NH4-Gain` -18,
-                        !is.na(`HCO2-Gain`) ~ `HCO2-Gain` - 45))
+  mutate(MW = case_when(!is.na(`H-Gain`) ~ `H-Gain` - H + electron,
+                        !is.na(`H-Loss`) ~ `H-Loss` + H - electron,
+                        !is.na(`NH4-Gain`) ~ `NH4-Gain` -(N + 4 * H) + electron,
+                        !is.na(`HCO2-Gain`) ~ `HCO2-Gain` - 45),
+         MW = round(MW, 4))
 
 RT_test <- Adduct_spread %>% 
   group_by(Name, Class, Formula, MW) %>% 
-  # filter(Reject != "rej") %>% 
   summarise(
     n = n(),
     meanRT = mean(RT),
     sdRT = sd(RT),
     minRT = min(RT),
     maxRT = max(RT)
-  )
+  ) %>% 
+  mutate(species = 
+  extract_TC(Name))  
 
 ggplot(RT_test, aes(meanRT, MW, colour = Class)) +
   geom_point()
 
+RT_test%>% filter(sdRT>2)
+
+Class_plot<- function(df, lipidClass, plotcolour = C){
+  df <- df %>% 
+    filter(Class == lipidClass) %>% 
+    mutate(C = str_extract(species, "\\d+(?=\\:)"),
+           DB = str_extract(species, "(?<=\\:)\\d+"))
+  plotcolour <- plotcolour
+  ggplot(df, aes(meanRT, MW, colour = DB)) +
+    geom_point() +
+    labs(title = lipidClass)
+}
+
+Class_plot(RT_test, "TG")
+Class_plot(RT_test, "CL")
+Class_plot(RT_test, "DG")
+Class_plot(RT_test, "PG")
+Class_plot(RT_test, "PE")
+Class_plot(RT_test, "PI")
+Class_plot(RT_test, "PC")
+Class_plot(RT_test, "PS")
+Class_plot(RT_test, "LPC")
+Class_plot(RT_test, "PA")
+Class_plot(RT_test, "Cer")
+Class_plot(RT_test, "CerPE")
+Class_plot(RT_test, "CerP")
+Class_plot(RT_test, "SM")
+Class_plot(RT_test, "SPH")
+Class_plot(RT_test, "FA")
+
+
+new_list <- RT_test %>% 
+  ungroup() %>% 
+  mutate(Formula = case_when(Class == "TG" ~ paste(Formula, "N", "H3", sep = " "),
+                             Class == "DG" ~ paste(Formula, "N", "H3", sep = " "),
+                             TRUE~Formula)) %>% 
+  arrange(Class, species)
+
 over_rep <- cleaned_batch %>% 
-  group_by(Name, Class, Formula, Ionization, Polarity, Adduct, ChargeState, ExtractedMass) %>% 
+  group_by(Name, Class, Formula, Ionization, Polarity, Adduct, ChargeState, ExtractedMass, batch) %>% 
   filter(Reject != "rej") %>% 
   summarise(
     n = n(),
@@ -208,6 +282,7 @@ plot_class <-  function(data, var =c("x")) {
     filter(Class %in% var)
   
   ggplot(data2, aes(meanRT, ExtractedMass)) +
+    geom_point(aes(colour)) +
     geom_point(aes(colour = batch, shape = Polarity)) +
     scale_shape_manual(values = c(6,16))
 }
@@ -216,4 +291,40 @@ plot_class <-  function(data, var =c("x")) {
 plot_class(over_rep02, var =c("PI","PG"))
 
 
+extract_TC(molecule)
 
+## a more dplyr like function  - but can't get it to work
+extract_species <- function(dt, molecule){
+  all_cols <- names(dt)[1:ncol(dt)]
+  df <- dt %>% 
+    ungroup() %>% 
+    select(molecule) %>% 
+    unique() %>% 
+    group_by(1)
+  df <- df %>% 
+    mutate(class = str_extract(1, "[a-zA-Z]+"),
+      # df <- as.data.frame(str_extract_all(molecule, "\\d+[:]\\d+")) %>%
+      #   separate(1, c("C", "DB"), ":", convert = T) %>%
+      #   summarise(
+      #     Carbons = sum(C),
+      #     DB = sum(DB)
+      #   )
+      C = sum(as.numeric(unlist(str_extract_all(1, "\\d+(?=\\:)")))),
+      DB = sum(as.numeric((unlist(str_extract_all(1, "(?<=\\:)\\d+"))))),
+      modification <- str_extract(str_extract(1, "(?<=\\().+?(?=\\))"), "[a-z]"),
+      # species <- ifelse(modification == "t"|modification == "d", paste(modification, df$Carbons, ":", df$DB, sep = ""), 
+      #                   ifelse(modification == "e"| modification == "p", paste(df$Carbons, modification, ":", df$DB, sep = ""), 
+      #                   paste(df$Carbons, df$DB, sep = ":")))
+      
+      species = case_when(modification == "t"|modification == "d" ~ paste(modification, C, ":", DB, sep = ""), 
+                           modification == "e"| modification == "p" ~  paste(C, modification, ":", DB, sep = ""), 
+                           TRUE ~ paste(C, DB, sep = ":")))
+  return(df)
+}
+
+extract_species(over_rep, Name)
+molecule <- filter(over_rep, grepl("TG", Name)) 
+molecule <- molecule$Name
+                      
+ C <- sum(as.numeric(unlist(str_extract("TG(18:1_18:2_16:0", "\\d+(?=\\:)"))))
+ 
