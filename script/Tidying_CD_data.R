6bb520a5744ae8884b16050a7ad857afa5e9f9bf
Colombo <col553@csiro.au>
2020-05-14 17:30:12 +1000

    cleaned script generalised

--- script/Tidying_CD_data.R
+++ script/Tidying_CD_data.R
@@ -30,13 +30,13 @@ extract_TC <- function(molecule){
 
 #read batch
 
-batch_01 <- read_tsv("Data/CD_results_Batch01._final_library.csv", col_types = cols(Name = col_character())) 
+batch <- read_tsv("Data/CD_results_Batch02._final_library.csv", col_types = cols(Name = col_character())) 
 #key <- read_csv("Data/masslist_for_CD.csv", col_types = cols(Name = col_character()))   %>% 
 #   select(Formula, species) 
   # unique()
 
 # run
-CDdata_gathered <- batch_01 %>% 
+CDdata_gathered <- batch %>% 
   gather(Sample, Area, contains("Area: ")) %>%
   select(Name, Formula, `Molecular Weight`, `RT [min]`, `Area (Max.)`, Sample, Area) %>% 
   unique() %>% 
@@ -50,6 +50,8 @@ CDdata_gathered <- batch_01 %>%
   filter(count >= 5) %>%
   mutate (pop = (str_extract(Sample, "\\w+_\\w+"))) %>% 
   separate(pop, c("population", "sample"), sep="(?<=[A-Za-z])(?=[0-9])") %>% 
+  mutate(population = tolower(population)) %>% 
+  filter(!is.na(population)) %>% 
          group_by(Name, Formula, `RT [min]`, `Area (Max.)`, Sample, Area, count) %>% 
            mutate(class = str_extract(`Name`, "[a-zA-Z]+"),
                   species = extract_TC(Name)) %>% 
@@ -76,12 +78,12 @@ CDdata_gathered <- batch_01 %>%
          pqn = Area2/quotient.median)
 
 var <-  unique(CDdata_gathered$class)
-#[1] "Cer"   "CerPE" "CL"    "DG"    "LPC"   "PA"    "PC"    "PE"    "PG"    "PI"    "PS"    "TG" 
-
+#[1]Batch 1 "Cer"   "CerPE" "CL"    "DG"    "LPC"   "PA"    "PC"    "PE"    "PG"    "PI"    "PS"    "TG" 
+#[1]Batch 2 "PC"    "TG"    "PE"    "CL"    "PS"    "CerPE" "PG"    "PA"    "LPC"   "PI"    "Cer"   "DG" 
 # sum the areas belonging to the same class of lipids (i.e. the areas of the different species)
 # batch_0420 is the final data for statistical analysis of ALL lipids based on normalised areas
 
-batch01_0520 <-  CDdata_gathered  %>% 
+batch_0520 <-  CDdata_gathered  %>% 
   group_by(class, Sample) %>%  
   mutate(sumpqn = sum(pqn))
  # mutate(population = tolower(population))
@@ -90,7 +92,7 @@ batch01_0520 <-  CDdata_gathered  %>%
 
 # plot batch 02 all lipids minus plasmalogens
 
-batch01_0520_plot <-  ggplot(batch01_0520, aes(`class`, log(sumpqn)))+
+batch_0520_plot <-  ggplot(batch_0520, aes(`class`, log(sumpqn)))+
   geom_boxplot(aes(fill = `population`), width =1) +
   scale_fill_brewer(palette = "Paired") +
   theme(plot.subtitle = element_text(vjust = 1), 
@@ -99,24 +101,46 @@ batch01_0520_plot <-  ggplot(batch01_0520, aes(`class`, log(sumpqn)))+
         axis.text.x = element_text(angle = 40), 
         panel.background = element_rect(fill = "grey91"), 
         legend.position = "right") +
-  labs(title = "Batch 01 Lipid Classes", y = "Log normalised area", x = "Lipid Classes") + 
+  labs(title = "Batch 02 Lipid Classes", y = "Log normalised area", x = "Lipid Classes") + 
   theme(axis.text = element_text(size = 8, face = "bold"), 
         axis.text.x = element_text(size = 10), 
         axis.text.y = element_text(size = 10), 
         legend.text = element_text(size = 11), 
         legend.title = element_text(size = 11)) 
 
-batch01_0520_plot
+batch_0520_plot
+
+# plots using the for loop 
+
+library(ggplot2)
+#lapply(var, function(x))
+
+df <- batch_0520
+var <-  unique(df$class)
 
 
-# working on  this plot now
+for (i in var) {
+      plot =  ggplot(data= subset(df, class == i)) +  
+          geom_boxplot(aes(x= species, y= log(pqn), fill = population )) +
+          ggtitle (i) +
+          theme(plot.subtitle = element_text(vjust = 1), 
+          plot.caption = element_text(vjust = 1), 
+          axis.text = element_text(size = 10, vjust = -0.25), 
+          axis.text.x = element_text(angle = 40))
+      
+      print(plot)
+}
+
+# ----------------------------------------------
+# old long version for plots here below
+
 df <- batch01_0520
 
 species_plot<- function(x){
   df <- df %>% 
     filter(class == x)
-    # mutate(C = str_extract(species, "\\d+(?=\\:)"),
-    #        DB = str_extract(species, "(?<=\\:)\\d+"))
+  # mutate(C = str_extract(species, "\\d+(?=\\:)"),
+  #        DB = str_extract(species, "(?<=\\:)\\d+"))
   #plotcolour <- plotcolour
   ggplot(df, aes(species, log(pqn))) +
     geom_boxplot(aes(fill = population))+
@@ -144,28 +168,5 @@ species_plot( "SM")
 species_plot( "SPH")
 species_plot( "FA")
 
-
-# same using the for loop 
-
-library(ggplot2)
-#lapply(var, function(x))
-
-df <- batch01_0520
-var <-  unique(df$class)
-
-for (i in var) {
-      plot =  ggplot(data= subset(df, class == i)) +  
-          geom_boxplot(aes(x= species, y= log(pqn), fill = population )) +
-          ggtitle (i) +
-          theme(plot.subtitle = element_text(vjust = 1), 
-          plot.caption = element_text(vjust = 1), 
-          axis.text = element_text(size = 10, vjust = -0.25), 
-          axis.text.x = element_text(angle = 40))
-      
-      print(plot)
-}
-
-
-
-
+# -------------------------------------------
 
