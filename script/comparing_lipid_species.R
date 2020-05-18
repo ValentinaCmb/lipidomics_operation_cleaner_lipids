3e1887012b79115d87088747be85d346b9809f0c
Colombo <col553@csiro.au>
2020-05-18 18:21:08 +1000

    updated and improved script that works

--- script/comparing_lipid_species.R
+++ script/comparing_lipid_species.R
@@ -4,112 +4,45 @@ library(RColorBrewer)
 library(modelr)
 library(stringr)
 
-extract_TC <- function(molecule){
-  lipidclass <- str_extract(molecule, "[a-zA-Z]+")
-  # df <- as.data.frame(str_extract_all(molecule, "\\d+[:]\\d+")) %>%
-  #   separate(1, c("C", "DB"), ":", convert = T) %>%
-  #   summarise(
-  #     Carbons = sum(C),
-  #     DB = sum(DB)
-  #   )
-  C <- sum(as.numeric(unlist(str_extract_all(molecule, "\\d+(?=\\:)"))))
-  DB <- sum(as.numeric((unlist(str_extract_all(molecule, "(?<=\\:)\\d+")))))
-  modification <- str_extract(str_extract(molecule, "(?<=\\().+?(?=\\))"), "[a-z]")
-  # species <- ifelse(modification == "t"|modification == "d", paste(modification, df$Carbons, ":", df$DB, sep = ""), 
-  #                   ifelse(modification == "e"| modification == "p", paste(df$Carbons, modification, ":", df$DB, sep = ""), 
-  #                   paste(df$Carbons, df$DB, sep = ":")))
-  
-  species <- case_when(modification == "t"|modification == "d" ~ paste(modification, C, ":", DB, sep = ""), 
-                       modification == "e"| modification == "p" ~  paste(C, ":", DB, modification, sep = ""), 
-                       TRUE ~ paste(C, DB, sep = ":"))
-  return(species)
-}
 
+batch_01 <- read_tsv("Data/CD_results_b01_unfiltered.csv", col_types = cols(Name = col_character())) 
 
-batch_01 <- read_tsv("Data/CD_results_b01_unfiltered.csv", col_types = cols(Name = col_character())) %>% 
-  # rename(RT01 = RT)%>% 
-  mutate(batch = "batch_01")
+batch_02 <- read_tsv("Data/CD_results_b02_unfiltered.csv", col_types = cols(Name = col_character())) 
 
-batch_02 <- read_tsv("Data/CD_results_b02_unfiltered.csv", col_types = cols(Name = col_character())) %>% 
-  # rename(RT02 = RT)%>% 
-  mutate(batch = "batch_02")
+batch_03 <- read_tsv("Data/CD_results_b03_unfiltered.csv", col_types = cols(Name = col_character())) 
+
+batch_04 <- read_tsv("Data/CD_results_b04_unfiltered.csv", col_types = cols(Name = col_character()))
+
 
 B01 <- batch_01 %>% 
   gather(Sample, Area, contains("Area: ")) %>%
-  select(Name, Formula, `Molecular Weight`, `RT [min]`, `Area (Max.)`, Sample, Area) %>% 
-  unique() %>% 
-  group_by(Name, Formula, `RT [min]`, `Area (Max.)`) %>% 
-  filter(!is.na(Area),
-         `RT [min]` < 25,
-         !is.na(Name)) %>% 
-  mutate(
-    count = n()
-  ) %>%  
-  mutate (pop = (str_extract(Sample, "\\w+_\\w+"))) %>% 
-  separate(pop, c("population", "sample"), sep="(?<=[A-Za-z])(?=[0-9])") %>% 
-  mutate(population = tolower(population)) %>% 
-  filter(!is.na(population)) %>% 
-  group_by(Name, Formula, `RT [min]`, `Area (Max.)`, Sample, Area, count) %>% 
-  mutate(class = str_extract(`Name`, "[a-zA-Z]+"),
-         species = extract_TC(Name)) %>% 
-  # mutate(Bond = str_extract(`FA Group Key`, "[a-zA-Z]+")) %>% 
-  mutate(TC = word(`species`, 1, 1, sep = ":"),
-         DB = word(`species`, -1, -1, sep = ":")) %>% 
-    group_by(Sample) %>%
-  mutate(Area2 = Area/sum(Area) * 1000000) %>% 
-  ungroup() %>% 
-  group_by(Formula, `Molecular Weight`, `RT [min]`) %>%
-  mutate(reference = median(Area2),
-         quotient = Area2/reference) %>%
-  ungroup() %>%
-  group_by(Sample) %>%
-  mutate(quotient.median = median(quotient),
-         pqn = Area2/quotient.median)%>% 
-  mutate(batch = "batch_01") %>% 
-  filter((class =="CL" & !`RT [min]` < 15) | (class =="TG" & !`RT [min]` < 10)) %>% 
-  ungroup() %>% 
-  # group_by(class, Sample) %>%  
-  # mutate(sumpqn = sum(pqn)) %>% 
-  # ungroup() %>% 
-  select(Name, RT_01 = `RT [min]`, population, sample, class, pqn, batch)
+  select(Name, RT_01 = `RT [min]`) %>% 
+  distinct(Name, .keep_all=TRUE)
 
 B02 <- batch_02 %>% 
   gather(Sample, Area, contains("Area: ")) %>%
-  select(Name, Formula, `Molecular Weight`, `RT [min]`, `Area (Max.)`, Sample, Area) %>% 
-  unique() %>% 
-  group_by(Name, Formula, `RT [min]`, `Area (Max.)`) %>% 
-  filter(!is.na(Area),
-         `RT [min]` < 25,
-         !is.na(Name)) %>% 
-  mutate(
-    count = n()
-  ) %>%  
-  mutate (pop = (str_extract(Sample, "\\w+_\\w+"))) %>% 
-  separate(pop, c("population", "sample"), sep="(?<=[A-Za-z])(?=[0-9])") %>% 
-  mutate(population = tolower(population)) %>% 
-  filter(!is.na(population)) %>% 
-  group_by(Name, Formula, `RT [min]`, `Area (Max.)`, Sample, Area, count) %>% 
-  mutate(class = str_extract(`Name`, "[a-zA-Z]+"),
-         species = extract_TC(Name)) %>% 
-  # mutate(Bond = str_extract(`FA Group Key`, "[a-zA-Z]+")) %>% 
-  mutate(TC = word(`species`, 1, 1, sep = ":"),
-         DB = word(`species`, -1, -1, sep = ":")) %>% 
-  group_by(Sample) %>%
-  mutate(Area2 = Area/sum(Area) * 1000000) %>% 
-  ungroup() %>% 
-  group_by(Formula, `Molecular Weight`, `RT [min]`) %>%
-  mutate(reference = median(Area2),
-         quotient = Area2/reference) %>%
-  ungroup() %>%
-  group_by(Sample) %>%
-  mutate(quotient.median = median(quotient),
-         pqn = Area2/quotient.median)%>% 
-  mutate(batch = "batch_01") %>% 
-  filter((class =="CL" & !`RT [min]` < 15) | (class =="TG" & !`RT [min]` < 10)) %>% 
-  ungroup() %>% 
-  # group_by(class, Sample) %>%  
-  # mutate(sumpqn = sum(pqn)) %>% 
-  # ungroup() %>% 
-  select(Name, RT_01 = `RT [min]`, population, sample, class, pqn, batch)
+  select(Name, RT_02 = `RT [min]`) %>% 
+  distinct(Name, .keep_all=TRUE)
+
 
 B12 <- inner_join(B01, B02, by= "Name")
+B12_not_common <- anti_join(B01, B02, by= "Name")
+
+# 
+
+B03 <- batch_03 %>% 
+  gather(Sample, Area, contains("Area: ")) %>%
+  select(Name, RT_03 = `RT [min]`) %>% 
+  distinct(Name, .keep_all=TRUE)
+
+B04 <- batch_04 %>% 
+  gather(Sample, Area, contains("Area: ")) %>%
+  select(Name, RT_04 = `RT [min]`) %>% 
+  distinct(Name, .keep_all=TRUE)
+
+
+B34 <- inner_join(B03, B04, by= "Name")
+B34_not_common <- anti_join(B03, B04, by= "Name")
+
+B12vs34_common <- inner_join(B12, B34, by= "Name")
+B12vs34_not_common <- anti_join(B12, B34, by= "Name")
